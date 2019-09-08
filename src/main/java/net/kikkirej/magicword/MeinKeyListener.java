ea951143f09bdeb41abe9b55af502ab1b482e7bf
Fabian Lemke <info@kikkirej.net>
2019-09-08 11:04:13 +0200

    LockScreen triggern mit "Escape"

--- src/main/java/net/kikkirej/magicword/MeinKeyListener.java
+++ src/main/java/net/kikkirej/magicword/MeinKeyListener.java
@@ -3,29 +3,32 @@ package net.kikkirej.magicword;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import net.kikkirej.magicword.actions.LockAction;
 import net.kikkirej.magicword.actions.NedryAction;
 
 public class MeinKeyListener implements KeyListener {
 
-	int[] chars = {KeyEvent.VK_P,
+	int[] password = {KeyEvent.VK_P,
 			KeyEvent.VK_I,
 			KeyEvent.VK_Z,
 			KeyEvent.VK_Z,
 			KeyEvent.VK_A};
-		
 	
 	int index = 0;
 	
 	@Override
 	public void keyPressed(KeyEvent arg0) {
 		if(NedryAction.didRun) {
+			if(arg0.getKeyCode() == KeyEvent.VK_ESCAPE){
+				new LockAction().run();
+			}
 			return;
 		}
 		arg0.getKeyCode();
 		try {
-			if(chars[index] == arg0.getKeyCode()){
+			if(password[index] == arg0.getKeyCode()){
 				index++;
-				if(chars.length == index+1) {
+				if(password.length == index+1) {
 					System.exit(0);
 				}
 			}else {
